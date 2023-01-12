27bd1367c12e11ff18110971d10fedb446560740
shushruthab <shushruthabhatt@gmail.com>
2023-01-11 22:16:17 -0800

    complete game development

---
+++ app.py
@@ -0,0 +1,41 @@
+from boggle import Boggle
+from flask import Flask, request, render_template, redirect, flash, jsonify, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "oh-so-secret"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+boggle_game = Boggle()
+
+@app.route('/')
+def home_page():
+
+    board = boggle_game.make_board()
+    session['board'] = board
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    return render_template("index.html", board=board,
+                           highscore=highscore,
+                           nplays=nplays)
+
+@app.route('/check-word')
+def word_checker():
+    word = request.args['word']
+    board = session['board']
+    resp = boggle_game.check_valid_word(board, word)
+    return jsonify({'result': resp})
+
+@app.route("/post-score", methods=["POST"])
+def post_score():
+    
+    score = request.json["score"]
+    highscore = session.get("highscore", 0)
+    nplays = session.get("nplays", 0)
+
+    session['nplays'] = nplays + 1
+    session['highscore'] = max(score, highscore)
+
+    return jsonify(brokeRecord=score > highscore)
